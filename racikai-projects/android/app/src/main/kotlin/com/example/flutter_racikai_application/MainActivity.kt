4b8dbeb05769b8ec756145a153886b84121374d0
EuphoriaBot <dimasfirja80@gmail.com>
2026-09-25 08:04:50 +0800

    Remove Duplicated Nested Projects

--- racikai-projects/android/app/src/main/kotlin/com/example/flutter_racikai_application/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_racikai_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- racikai-projects/lib/features/recipe/domain/repositories/recipe_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../entities/recipe_entity.dart';
-
-abstract class RecipeRepository {
-  Future<List<RecipeEntity>> getRecipes();
-
-  Future<RecipeEntity?> getRecipeById(int id);
-}
